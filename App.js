f1f61a8f11f95c2e3fe7b0a9f47732c23d031289
Chris <cgormley1275@gmail.com>
2024-11-23 15:59:57 -0500

    started A6 up to 3.3

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import express from 'express';
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
@@ -28,6 +29,8 @@ if (process.env.NODE_ENV !== "development") {
         domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 app.use(session(sessionOptions));
 app.use(express.json());
 ModuleRoutes(app);
