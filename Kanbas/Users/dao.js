fdd5ae651c4aaff2824f58f5e351551d1681f0cb
Chris <cgormley1275@gmail.com>
2024-11-26 08:55:21 -0500

    fixed changes made to test deployment

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -7,17 +7,7 @@ export const createUser = (user) => {
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) => model.findOne({ username: username });
-// export const findUserByCredentials = (username, password) => model.findOne({ username, password });
-//testing
-export const findUserByCredentials = (username, password) => {
-    data = model.findOne({ username, password });
-    if (!data) {
-        throw("no data!!!")
-    }
-}
-    
-    
-//testing
+export const findUserByCredentials = (username, password) => model.findOne({ username, password });
 export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
 export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
